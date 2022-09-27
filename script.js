bf11d619e9fe0c80dfcc38fed3680f4a19f31657
rohsikdnan <90635399+rohsikdnan@users.noreply.github.com>
2022-09-27 23:27:38 +0530

    more functions and css added

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 let blockDivContainer = document.querySelector('#blockDivContainer'),
     inpbtns = document.getElementById('btns'),
-    blockLine = document.querySelector('#blockLine'),
+    character = document.querySelectorAll('#blockLine span'),
     goBtn = document.querySelector('#go'),
     lessons = document.querySelectorAll('.lessons'),
     timevalue = document.querySelector('.time'),
-    speed = document.querySelector('.speed'),
     accuracyValue = document.querySelector('.acc'),
-    flag = mistake = 0,
+    pCompleteValue = document.querySelector('.pCompleteValue'),
+    resetBtn = document.querySelector('#resetBtn'),
+    inputField = document.querySelector('#inputField'),
+    resultContainer = document.querySelector('#resultContainer'),
+
+    accuracy = document.querySelector('#accuracy'),
+    speed = document.querySelector('#speed'),
+    correctLetters = document.querySelector('#correctLetters'),
+    inCorrectLetters = document.querySelector('#inCorrectLetters'),
+    fixedLetters = document.querySelector('#fixedLetters'),
+    totalLetters = document.querySelector('#totalLetters'),
+    flag = mistake = charIndex = typedLetters = 0,
     time = 60,
     timeLimit = time,
-    timer, accuracy, wpm;
+    timer, wpm, completedLetters, inCorrectLetterN  = 0;
+
+
 
 //adding function to print lessons
-lessons.forEach(e => {
-    e.onclick = () => printkeys(e.textContent);
+lessons.forEach(element => {
+    element.onclick = () => printkeys(element.textContent);
 });
 
 inpbtns.value = 'fj';
@@ -32,16 +44,7 @@ goBtn.addEventListener('click', () => {
     printkeys(keybtns);
 });
 
-//funtion for printing letters
-function printkeys(keybtns) {
-    blockDivContainer.removeEventListener('keydown', checkValue);
-    blockDivContainer.addEventListener('keydown', checkValue);
-    clearInterval(timer);
-    timevalue.innerText = time;
-    accuracyValue.innerText = '100';
-    speed.innerText = 00;
-    flag = mistake = 0;
-
+function randomPara(letters) {
     var alreadyDone = [];
     randomValueFromArray = (myArray) => {
         if (alreadyDone.length === 0) {
@@ -54,20 +57,40 @@ function printkeys(keybtns) {
         return myArray[indexOfItemInMyArray];
     };
     //inserting first letter
-    let b = Math.floor(Math.random() * keybtns.length);
-    blockLine.innerHTML = `<span class="nxtLetter">${keybtns[b]}</span>`;
-    
+    let b = Math.floor(Math.random() * letters.length);
+    let para = letters[b];
+
     let i = 1;
     while (i < 200) {
         let d = randomValueFromArray(["1", "2", "3", "4", "5", "0", "6"]);
         if (d == 0) {
-            blockLine.innerHTML += `<span class="plainText space"> </span>`;
+            para += ' ';
         } else {
-            b = Math.floor(Math.random() * keybtns.length);
-            blockLine.innerHTML += `<span class="plainText">${keybtns[b]}</span>`;
+            b = Math.floor(Math.random() * letters.length);
+            para += letters[b];
         }
         i++;
     }
+    return para;
+}
+//funtion for printing letters
+function printkeys(keybtns) {
+    resetBtn.setAttribute('onclick', `printkeys("${keybtns}")`);
+    reset();
+
+
+    let paragraph = randomPara(keybtns);
+    paragraph.split("  ").join("").split("").forEach(element => {
+        blockLine.innerHTML += `<span>${element}</span>`;
+    });
+    let char = blockLine.querySelectorAll('span');
+    char[0].classList.add('active');
+    char.forEach(element => {
+        if (element.innerText == ' ') element.classList.add('space');
+    });
+    // inputField.setAttribute("maxlength",char.length);
+    correctLetters.innerText = char.length;
+    totalLetters.innerText = char.length;
 }
 
 printkeys('fj');
@@ -75,21 +98,18 @@ printkeys('fj');
 // check letter is correct/incorrect
 function checkValue(e) {
     e.preventDefault();
-    let nxtLetter = document.querySelector('.nxtLetter');
-    let nxtLetterPre = nxtLetter.previousElementSibling;
-    let text = nxtLetter.textContent;
+    let inputCharacter = inputField.value.split('')[charIndex];
+    let character = blockLine.querySelectorAll('span');
     //backspace
-    if (e.keyCode == 8) {
-        if (nxtLetterPre) {
-            nxtLetterPre.classList.replace('bad_Space', 'plainText');
-            nxtLetterPre.classList.replace('bad_Entry', 'plainText');
-            nxtLetterPre.classList.add('nxtLetter');
-            nxtLetter.classList.replace('nxtLetter', 'plainText');
-            if (!nxtLetterPre.classList.contains('goodEntry')) {
-                mistake--;
-            }
-            nxtLetterPre.classList.remove('goodEntry');
-        }
+
+    if (inputCharacter == null) {
+        charIndex--;
+
+        if (!character[charIndex].classList.contains('correct'))
+            mistake--;
+        character[charIndex].classList.remove('incorrect', 'correct');
+        completedLetters--;
+
     }
     else {
         if (flag == 0) {
@@ -98,26 +118,42 @@ function checkValue(e) {
             flag = 1;
         }
 
-        if (text == e.key)
-            nxtLetter.classList.replace('nxtLetter', 'goodEntry');
+        if (character[charIndex].innerText == inputCharacter || (character[charIndex].innerHTML == ' ' && inputCharacter == ' '))
+            character[charIndex].classList.add('correct');
+
         else {
-            nxtLetter.classList.replace('nxtLetter', text == " " ? 'bad_Space' : 'bad_Entry');
+            character[charIndex].classList.add('incorrect');
             mistake++;
+            inCorrectLetterN++;
         }
-        if (nxtLetter.nextElementSibling) {
-            nxtLetter.nextElementSibling.classList.replace('plainText', 'nxtLetter');
-        } else {
-            clearInterval(timer);
-            blockDivContainer.removeEventListener('keydown', checkValue);
-        }
+
+    typedLetters++;
+    completedLetters++;
+        charIndex++;
     }
-    let char = document.querySelectorAll('#blockLine span');
-    let charIndex = char.length;
-    accuracy = Math.floor(((charIndex - mistake) / charIndex) * 100);
-    wpm = Math.round(((((charIndex - mistake) / 5) / (time - timeLimit)) * 60));
-    wpm = wpn = 0 || wpm === Infinity || !wpm ? 0 : wpm;
-    accuracyValue.innerText = accuracy;
-    speed.innerText = wpm;
+    character.forEach(element => { element.classList.remove('active') });
+    if (character[charIndex]) {
+        character[charIndex].classList.add('active');
+    } else {
+        // inputField.disabled = true;
+        clearInterval(timer);
+        inputField.removeEventListener('input', checkValue);
+        resultContainer.style.height = 'auto';
+        resultContainer.style.display = 'block';
+    }
+    let totalLetter = character.length;
+    let accuracyc = (((totalLetter - mistake) / totalLetter) * 100).toFixed(1);
+    wpm = ((typedLetters/5 - mistake) / (time - timeLimit)*60);
+    wpm = (wpn = 0 || wpm === Infinity || !wpm) ? 0 : wpm;
+    pComplete = (completedLetters / totalLetter * 100).toFixed(1);
+    pCompleteValue.innerText = pComplete + '%';
+    accuracyValue.innerText = accuracyc + '%';
+
+    accuracy.innerText = accuracyc + '%';
+    // speed.innerText = wpm + ' wpm';
+    correctLetters.innerText = totalLetter - mistake;
+    inCorrectLetters.innerText = inCorrectLetterN;
+    fixedLetters.innerText = inCorrectLetterN - mistake;
 }
 
 //timer
@@ -133,6 +169,30 @@ function timerUpdate() {
     }
     else {
         clearInterval(timer);
-        blockDivContainer.removeEventListener('keydown', checkValue);
+        resultContainer.style.height = 'auto';
+        resultContainer.style.display = "block";
+        inputField.removeEventListener('input', checkValue);
     }
 }
+
+function reset() {
+    blockLine.innerHTML = '';
+    inputField.value = '';
+    inputField.removeEventListener('input', checkValue);
+    inputField.addEventListener('input', checkValue);
+    clearInterval(timer);
+    timevalue.innerText = time;
+    accuracyValue.innerText = '100.0%';
+    pCompleteValue.innerText = '0%';
+    accuracy.innerText = '100 %';
+    speed.innerText = '0 wpm';
+    correctLetters.innerText = 200;
+    inCorrectLetters.innerText = 0;
+    fixedLetters.innerText = 0;
+    flag = mistake = completedLetters = charIndex =typedLetters= 0;
+    resultContainer.style.height = 0;
+    resultContainer.style.display = "none";
+}
+
+blockDivContainer.addEventListener("click", () => inputField.focus());
+
